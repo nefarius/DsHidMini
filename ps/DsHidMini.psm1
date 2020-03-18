c7c014d33c91f5dd6c66a3705fa7695d3c742476
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2020-03-18 20:40:26 +0100

    Cleaned up ps

---
+++ ps/DsHidMini.psm1
@@ -0,0 +1,273 @@
+Import-Module .\Invoke-Win32Api.psm1
+
+Add-Type -TypeDefinition @"
+namespace DS {
+	using System;
+	using System.Runtime.InteropServices;
+	
+	public enum DS_DEVICE_TYPE : uint
+	{
+		//
+		// Unknown device type
+		// 
+		DS_DEVICE_TYPE_UNKNOWN = 0x00,
+
+		//
+		// Sony DualShock 3 Controller
+		// 
+		DS_DEVICE_TYPE_PS3_DUALSHOCK,
+
+		//
+		// Sony DualShock 4 Controller
+		// 
+		DS_DEVICE_TYPE_PS4_DUALSHOCK,
+
+		//
+		// Sony Navigation Controller
+		// 
+		DS_DEVICE_TYPE_PS3_NAVIGATION,
+
+		//
+		// Sony Motion Controller
+		// 
+		DS_DEVICE_TYPE_PS3_MOTION
+	}
+	
+	public enum DS_CONNECTION_TYPE : uint
+	{
+		//
+		// Unknown connection type
+		// 
+		DS_CONNECTION_TYPE_UNKNOWN = 0x00,
+
+		//
+		// Connected via USB (wired)
+		// 
+		DS_CONNECTION_TYPE_USB = 0x01,
+
+		//
+		// Connected via Bluetooth (wireless)
+		// 
+		DS_CONNECTION_TYPE_BLUETOOTH = 0x02
+	}
+	
+	public enum DS_FEATURE_TYPE : uint
+	{
+		//
+		// Receive controller-embedded Bluetooth host address
+		// 
+		DS_FEATURE_TYPE_GET_HOST_BD_ADDR = 0xC0,
+
+		//
+		// Update controller-embedded Bluetooth host address
+		// 
+		DS_FEATURE_TYPE_SET_HOST_BD_ADDR = 0xC1,
+
+		//
+		// Receive controller-embedded Bluetooth address
+		// 
+		DS_FEATURE_TYPE_GET_DEVICE_BD_ADDR = 0xC2,
+
+		//
+		// Receive device type (DS3, DS4, ...)
+		// 
+		DS_FEATURE_TYPE_GET_DEVICE_TYPE = 0xC3,
+
+		//
+		// Receive device connection type (USB, Bluetooth)
+		// 
+		DS_FEATURE_TYPE_GET_CONNECTION_TYPE = 0xC4,
+
+		//
+		// Receive current volatile configuration properties
+		// 
+		DS_FEATURE_TYPE_GET_DEVICE_CONFIG = 0xC5,
+
+		//
+		// Update current volatile configuration properties
+		// 
+		DS_FEATURE_TYPE_SET_DEVICE_CONFIG = 0xC6
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct BD_ADDR
+	{
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst=6)]
+		public byte[] Address;
+	}
+	
+	public enum DS_HID_DEVICE_MODE : uint
+	{
+		DS_HID_DEVICE_MODE_SINGLE,
+
+		DS_HID_DEVICE_MODE_MULTI
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_DRIVER_CONFIGURATION
+	{
+		public DS_HID_DEVICE_MODE HidDeviceMode;
+
+		public bool MuteDigitalPressureButtons;
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_HEADER
+	{
+		public byte ReportId;
+
+		public UInt32 Size;
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_GET_HOST_BD_ADDR
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public BD_ADDR HostAddress;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_GET_HOST_BD_ADDR;
+		}
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_SET_HOST_BD_ADDR
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public BD_ADDR HostAddress;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_SET_HOST_BD_ADDR;
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_GET_DEVICE_BD_ADDR
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public BD_ADDR DeviceAddress;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_GET_DEVICE_BD_ADDR;
+		}
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_GET_DEVICE_TYPE
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public DS_DEVICE_TYPE DeviceType;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_GET_DEVICE_TYPE;
+		}
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_GET_CONNECTION_TYPE
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public DS_CONNECTION_TYPE ConnectionType;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_GET_CONNECTION_TYPE;
+		}
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_GET_DEVICE_CONFIG
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public DS_DRIVER_CONFIGURATION Configuration;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_GET_DEVICE_CONFIG;
+		}
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Pack=1)]
+	public struct DS_FEATURE_SET_DEVICE_CONFIG
+	{
+		public DS_FEATURE_HEADER Header;
+
+		public DS_DRIVER_CONFIGURATION Configuration;
+		
+		public void Init()
+		{
+			this.Header.Size = (UInt32)Marshal.SizeOf(this.GetType());
+			this.Header.ReportId = (byte)DS_FEATURE_TYPE.DS_FEATURE_TYPE_SET_DEVICE_CONFIG;
+		}
+	}
+}
+"@;
+
+function Invoke-CreateFileW([string]$devicePath)
+{
+	$handle = Invoke-Win32Api -DllName kernel32.dll `
+		-MethodName CreateFileW `
+		-ReturnType Microsoft.Win32.SafeHandles.SafeFileHandle `
+		-ParameterTypes @([String], [System.Security.AccessControl.FileSystemRights], [System.IO.FileShare], [IntPtr], [System.IO.FileMode], [UInt32], [IntPtr]) `
+		-Parameters @(
+			$devicePath,
+			([System.Security.AccessControl.FileSystemRights]::Read, [System.Security.AccessControl.FileSystemRights]::Write),
+			[System.IO.FileShare]::ReadWrite,
+			[IntPtr]::Zero,
+			[System.IO.FileMode]::Open,
+			0,
+			[IntPtr]::Zero) `
+		-SetLastError $true `
+		-CharSet Unicode
+		
+	if ($handle.IsInvalid) {
+		$last_err = [System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+		throw [System.ComponentModel.Win32Exception]$last_err
+	}
+	
+	return $handle
+}
+
+function Get-HidFeature([Microsoft.Win32.SafeHandles.SafeFileHandle]$handle, [IntPtr]$buffer, [UInt32]$length)
+{
+	return Invoke-Win32Api -DllName hid.dll `
+		-MethodName HidD_GetFeature `
+		-ReturnType System.Boolean `
+		-ParameterTypes @([Microsoft.Win32.SafeHandles.SafeFileHandle], [IntPtr], [UInt32]) `
+		-Parameters @(
+			$handle,
+			$buffer,
+			$length) `
+		-SetLastError $true `
+		-CharSet Unicode
+}
+
+function Set-HidFeature([Microsoft.Win32.SafeHandles.SafeFileHandle]$handle, [IntPtr]$buffer, [UInt32]$length)
+{
+	return Invoke-Win32Api -DllName hid.dll `
+		-MethodName HidD_SetFeature `
+		-ReturnType System.Boolean `
+		-ParameterTypes @([Microsoft.Win32.SafeHandles.SafeFileHandle], [IntPtr], [UInt32]) `
+		-Parameters @(
+			$handle,
+			$buffer,
+			$length) `
+		-SetLastError $true `
+		-CharSet Unicode
+}
--- ps/Playground.ps1
+++ ps/Playground.ps1
@@ -1,75 +1,16 @@
-Import-Module .\Invoke-Win32Api.psm1
-Import-Module .\PSReflect.psm1
+Import-Module .\DsHidMini.psm1
 
 $devicePath = "\\?\hid#vid_054c&pid_0268#9&2d65d5ae&5&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"
 
-$handle = Invoke-Win32Api -DllName kernel32.dll `
-	-MethodName CreateFileW `
-	-ReturnType Microsoft.Win32.SafeHandles.SafeFileHandle `
-	-ParameterTypes @([String], [System.Security.AccessControl.FileSystemRights], [System.IO.FileShare], [IntPtr], [System.IO.FileMode], [UInt32], [IntPtr]) `
-	-Parameters @(
-		$devicePath,
-		([System.Security.AccessControl.FileSystemRights]::Read, [System.Security.AccessControl.FileSystemRights]::Write),
-		[System.IO.FileShare]::ReadWrite,
-		[IntPtr]::Zero,
-		[System.IO.FileMode]::Open,
-		0,
-		[IntPtr]::Zero) `
-	-SetLastError $true `
-	-CharSet Unicode
-if ($handle.IsInvalid) {
-	$last_err = [System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
-	throw [System.ComponentModel.Win32Exception]$last_err
-}
+$handle = Invoke-CreateFileW $devicePath
 
-$buffer = [System.Runtime.InteropServices.Marshal]::AllocHGlobal(11)
-[System.Runtime.InteropServices.Marshal]::WriteByte($buffer, 0, 192)
+$getConnectionType = [System.Activator]::CreateInstance([DS.DS_FEATURE_GET_CONNECTION_TYPE])
+$getConnectionType.Init()
+$buffer = [System.Runtime.InteropServices.Marshal]::AllocHGlobal($getConnectionType.Header.Size)
+[System.Runtime.InteropServices.Marshal]::StructureToPtr($getConnectionType, $buffer, $false)
 
-function Get-HidFeature([Microsoft.Win32.SafeHandles.SafeFileHandle]$handle, [IntPtr]$buffer, [UInt32]$length)
-{
-	return Invoke-Win32Api -DllName hid.dll `
-		-MethodName HidD_GetFeature `
-		-ReturnType System.Boolean `
-		-ParameterTypes @([Microsoft.Win32.SafeHandles.SafeFileHandle], [IntPtr], [UInt32]) `
-		-Parameters @(
-			$handle,
-			$buffer,
-			$length) `
-		-SetLastError $true `
-		-CharSet Unicode
-}
+Get-HidFeature $handle $buffer $getConnectionType.Header.Size
 
-function Set-HidFeature([Microsoft.Win32.SafeHandles.SafeFileHandle]$handle, [IntPtr]$buffer, [UInt32]$length)
-{
-	return Invoke-Win32Api -DllName hid.dll `
-		-MethodName HidD_SetFeature `
-		-ReturnType System.Boolean `
-		-ParameterTypes @([Microsoft.Win32.SafeHandles.SafeFileHandle], [IntPtr], [UInt32]) `
-		-Parameters @(
-			$handle,
-			$buffer,
-			$length) `
-		-SetLastError $true `
-		-CharSet Unicode
-}
-
-$mod = New-InMemoryModule
-
-$DsConnectionType = psenum $mod DS.DS_CONNECTION_TYPE UInt32 @{
-	DS_CONNECTION_TYPE_UNKNOWN = 	0x00
-	DS_CONNECTION_TYPE_USB =		0x01
-	DS_CONNECTION_TYPE_BLUETOOTH = 	0x02
-}
-
-$GetConnectionType = struct $mod DS.DS_FEATURE_GET_CONNECTION_TYPE @{
-	ReportId =			field 0 Byte
-	Size =				field 1 UInt32
-	ConnectionType = 	field 2 $DsConnectionType
-}
-
-$test = [System.Activator]::CreateInstance($GetConnectionType)
-[System.Runtime.InteropServices.Marshal]::SizeOf([Type]$DsConnectionType)
-$test.Size = [System.Runtime.InteropServices.Marshal]::SizeOf([Type]$GetConnectionType)
-
+[System.Runtime.InteropServices.Marshal]::FreeHGlobal($buffer)
 
 $handle.Close()
