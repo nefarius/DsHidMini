f26af474af3873c7e214392394c0d877a7788777
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2020-03-22 21:02:43 +0100

    Added DsHidMiniModule

---
+++ ps/DsHidMiniModule/DsHidMiniModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace DsHidMiniModule
+{
+    [Cmdlet(VerbsCommon.Get, "FireShockDevice")]
+    [OutputType(typeof(int))]
+    public class GetFireShockDevice : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            
+        }
+    }
+}
