d8a11202d6d631f3f4f09059bfcbeeb18f1ff790
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2020-03-23 15:36:34 +0100

    Updated example

--- ps/Playground.ps1
+++ ps/Playground.ps1
@@ -1,7 +1,6 @@
 Import-Module .\DsHidMini.psm1
 
-$devicePath = "\\?\hid#vid_054c&pid_0268#9&2d65d5ae&7&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"
-#$devicePath = "\\?\hid#vid_054c&pid_0268#9&2d65d5ae&5&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"
+$devicePath = ((Get-HidDevice)[0]).DeviceId
 
 $handle = Invoke-CreateFileW $devicePath
 
