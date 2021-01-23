b384f7535f61344e76d3715ad5c38075d8f5c4a4
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-23 13:42:24 +0100

    Added missing macros

--- sys/Trace.h
+++ sys/Trace.h
@@ -43,11 +43,26 @@ Environment:
         WPP_DEFINE_BIT(TRACE_CONFIG)                                   \
         )                             
 
+
+#define WPP_FLAG_LEVEL_LOGGER(flag, level)                              \
+    WPP_LEVEL_LOGGER(flag)
+
+#define WPP_FLAG_LEVEL_ENABLED(flag, level)                             \
+    (WPP_LEVEL_ENABLED(flag) &&                                         \
+     WPP_CONTROL(WPP_BIT_ ## flag).Level >= level)
+
+#define WPP_LEVEL_FLAGS_LOGGER(lvl,flags) \
+           WPP_LEVEL_LOGGER(flags)
+
+#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) \
+           (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
+
+//
 // This comment block is scanned by the trace preprocessor to define our
 // Trace function.
 //
 // USEPREFIX and USESUFFIX strip all trailing whitespace, so we need to surround
-// FuncExit messages with brackets
+// FuncExit messages with brackets 
 //
 // begin_wpp config
 // FUNC Trace{FLAG=MYDRIVER_ALL_INFO}(LEVEL, MSG, ...);
