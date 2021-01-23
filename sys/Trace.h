d44ba06f3439a0759ec3ccc06f8062007fe5916c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-23 13:04:53 +0100

    Added DMF tracing

--- sys/Trace.h
+++ sys/Trace.h
@@ -22,8 +22,14 @@ Environment:
 //
 
 #define WPP_CONTROL_GUIDS                                              \
+    /* {F59F6F9B-A539-4846-9DE8-EFC2C43D0A91} */ \
+    WPP_DEFINE_CONTROL_GUID( \
+        DmfLibraryTraceGuid, (F59F6F9B,A539,4846,9DE8,EFC2C43D0A91), \
+        WPP_DEFINE_BIT(DMF_TRACE) \
+    ) \
+                                                                       \
     WPP_DEFINE_CONTROL_GUID(                                           \
-        MyDriver1TraceGuid, (4eefa829,ba06,44ff,906e,8082a387206f),                  \
+        DsHidMiniTraceGuid, (4eefa829,ba06,44ff,906e,8082a387206f),                  \
                                                                        \
         WPP_DEFINE_BIT(MYDRIVER_ALL_INFO)                              \
         WPP_DEFINE_BIT(TRACE_DRIVER)                                   \
@@ -37,27 +43,34 @@ Environment:
         WPP_DEFINE_BIT(TRACE_CONFIG)                                   \
         )                             
 
-#define WPP_FLAG_LEVEL_LOGGER(flag, level)                             \
-    WPP_LEVEL_LOGGER(flag)
-
-#define WPP_FLAG_LEVEL_ENABLED(flag, level)                            \
-    (WPP_LEVEL_ENABLED(flag) &&                                        \
-     WPP_CONTROL(WPP_BIT_ ## flag).Level >= level)
-
-#define WPP_LEVEL_FLAGS_LOGGER(lvl,flags)                              \
-           WPP_LEVEL_LOGGER(flags)
-               
-#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags)                            \
-           (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
-
-//
 // This comment block is scanned by the trace preprocessor to define our
 // Trace function.
+//
+// USEPREFIX and USESUFFIX strip all trailing whitespace, so we need to surround
+// FuncExit messages with brackets
 //
 // begin_wpp config
 // FUNC Trace{FLAG=MYDRIVER_ALL_INFO}(LEVEL, MSG, ...);
 // FUNC TraceEvents(LEVEL, FLAGS, MSG, ...);
+// FUNC FuncEntry{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// FUNC FuncEntryArguments{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExit{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExitVoid{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// FUNC TraceError{LEVEL=TRACE_LEVEL_ERROR}(FLAGS, MSG, ...);
+// FUNC TraceInformation{LEVEL=TRACE_LEVEL_INFORMATION}(FLAGS, MSG, ...);
+// FUNC TraceVerbose{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExitNoReturn{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
 // FUNC TraceDbg{LEVEL=TRACE_LEVEL_INFORMATION}(FLAGS, MSG, ...);
+// USEPREFIX(FuncEntry, "%!STDPREFIX! [%!FUNC!] --> Entry");
+// USEPREFIX(FuncEntryArguments, "%!STDPREFIX! [%!FUNC!] --> Entry <");
+// USEPREFIX(FuncExit, "%!STDPREFIX! [%!FUNC!] <-- Exit <");
+// USESUFFIX(FuncExit, ">");
+// USEPREFIX(FuncExitVoid, "%!STDPREFIX! [%!FUNC!] <-- Exit");
+// USEPREFIX(TraceError, "%!STDPREFIX! [%!FUNC!] ERROR:");
+// USEPREFIX(TraceEvents, "%!STDPREFIX! [%!FUNC!] ");
+// USEPREFIX(TraceInformation, "%!STDPREFIX! [%!FUNC!] ");
+// USEPREFIX(TraceVerbose, "%!STDPREFIX! [%!FUNC!] ");
+// USEPREFIX(FuncExitNoReturn, "%!STDPREFIX! [%!FUNC!] <--");
 // end_wpp
 
 //
