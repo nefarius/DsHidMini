b7b32c172c4526c0f57571acf85031cbcc3fcdbe
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-30 00:02:57 +0100

    Added Costura

---
+++ DSHMC/DsHidMiniDriver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSHMC
+{
+    public static class DsHidMiniDriver
+    {
+        public static Guid DeviceInterfaceGuid => Guid.Parse("{399ED672-E0BD-4FB3-AB0C-4955B56FB86A}");
+    }
+}
