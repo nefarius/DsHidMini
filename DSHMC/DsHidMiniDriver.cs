76c720625691b14e831450b2419d8a70cfe290c5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-02-02 20:51:35 +0100

    Tested property reading

--- DSHMC/DsHidMiniDriver.cs
+++ DSHMC/DsHidMiniDriver.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nefarius.Devcon;
 
 namespace DSHMC
 {
     public static class DsHidMiniDriver
     {
         public static Guid DeviceInterfaceGuid => Guid.Parse("{399ED672-E0BD-4FB3-AB0C-4955B56FB86A}");
+
+        public static DevicePropertyKey BatteryStatusProperty => CustomDeviceProperty.CreateCustomDeviceProperty(
+            Guid.Parse("{0x52fac1da, 0x5a52, 0x40f5, {0xa1, 0x23, 0x36, 0x7f, 0x76, 0xf, 0x8b, 0xc2}}"), 2,
+            typeof(byte));
+
+        public static DevicePropertyKey HidDeviceModeProperty => CustomDeviceProperty.CreateCustomDeviceProperty(
+            Guid.Parse("{0x52fac1da, 0x5a52, 0x40f5, {0xa1, 0x23, 0x36, 0x7f, 0x76, 0xf, 0x8b, 0xc2}}"), 3,
+            typeof(byte));
+
+        public enum DsBatteryStatus : byte
+        {
+            None = 0x00,
+            Dying = 0x01,
+            Low = 0x02,
+            Medium = 0x03,
+            High = 0x04,
+            Full = 0x05,
+            Charging = 0xEE,
+            Charged = 0xEF
+        }
+
+        public enum DsHidDeviceMode : byte
+        {
+            Unknown = 0x00,
+            Single,
+            Multi,
+            SixaxisCompatible
+        }
     }
 }
--- DSHMC/MainWindow.xaml.cs
+++ DSHMC/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using AdonisUI.Controls;
+﻿using System.Threading.Tasks;
+using AdonisUI.Controls;
+using Nefarius.Devcon;
 
 namespace DSHMC
 {
@@ -10,6 +12,28 @@ namespace DSHMC
         public MainWindow()
         {
             InitializeComponent();
+
+            Task.Run(Action);
+        }
+
+        private void Action()
+        {
+            var instance = 0;
+
+            while (Devcon.Find(
+                DsHidMiniDriver.DeviceInterfaceGuid,
+                out var path,
+                out var instanceId,
+                instance++))
+            {
+                var device = Device.GetDeviceByInstanceId(instanceId);
+
+                var battery =
+                    (DsHidMiniDriver.DsBatteryStatus) device.GetProperty<byte>(DsHidMiniDriver.BatteryStatusProperty);
+
+                var mode =
+                    (DsHidMiniDriver.DsHidDeviceMode) device.GetProperty<byte>(DsHidMiniDriver.HidDeviceModeProperty);
+            }
         }
     }
 }
