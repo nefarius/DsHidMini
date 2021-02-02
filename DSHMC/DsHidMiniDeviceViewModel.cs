dd9e5e82da1dd030e0028f82eca89fd879e25397
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-02-02 23:33:47 +0100

    Added PropertyChanged weaver

---
+++ DSHMC/DsHidMiniDeviceViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DSHMC.Annotations;
+
+namespace DSHMC
+{
+    class DsHidMiniDeviceViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
