4d94262a712b00ba32cc71a84e64679712b2e1d2
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-03-05 11:21:28 +0100

    Added struct _DS3_RAW_INPUT_REPORT

--- sys/Ds3.h
+++ sys/Ds3.h
@@ -143,6 +143,36 @@ VOID FORCEINLINE DS3_SET_LARGE_RUMBLE_STRENGTH(
 	}
 }
 
+#include <pshpack1.h>
+
+typedef struct _DS3_RAW_INPUT_REPORT
+{
+	UCHAR ReportId;
+
+	UCHAR Reserved0;
+
+	ULONG Buttons;
+
+	UCHAR LeftThumbX;
+	UCHAR LeftThumbY;
+
+	UCHAR RightThumbX;
+	UCHAR RightThumbY;
+
+	UCHAR Reserved1[3];
+
+	UCHAR PressureValues[12];
+
+	UCHAR Reserved2[16];
+
+	USHORT AccelerometerX;
+	USHORT AccelerometerY;
+	USHORT AccelerometerZ;
+	USHORT Gyroscope;
+	
+} DS3_RAW_INPUT_REPORT, *PDS3_RAW_INPUT_REPORT;
+
+#include <poppack.h>
 
 typedef enum _USB_HID_REQUEST
 {
