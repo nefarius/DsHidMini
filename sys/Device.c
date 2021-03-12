990041352cf537217776e784182965e08024f780
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-03-12 12:51:25 +0100

    Added comment

--- sys/Device.c
+++ sys/Device.c
@@ -444,7 +444,7 @@ DmfDeviceModulesAdd(
 	dmfSchedulerCfg.CallbackContext = pDevCtx;
 	dmfSchedulerCfg.PersistenceType = ScheduledTask_Persistence_NotPersistentAcrossReboots;
 	dmfSchedulerCfg.ExecutionMode = ScheduledTask_ExecutionMode_Deferred;
-	dmfSchedulerCfg.ExecuteWhen = ScheduledTask_ExecuteWhen_Other;
+	dmfSchedulerCfg.ExecuteWhen = ScheduledTask_ExecuteWhen_Other; // Don't use timer
 	dmfSchedulerCfg.TimeMsBeforeInitialCall = 1000;
 	dmfSchedulerCfg.TimerPeriodMsOnSuccess = pDevCtx->Configuration.OutputReportTimerPeriodMs;
 	dmfSchedulerCfg.TimerPeriodMsOnFail = pDevCtx->Configuration.OutputReportTimerPeriodMs;
