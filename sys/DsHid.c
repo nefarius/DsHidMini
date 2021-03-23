92225d72544d1eebb53cdfb5ac8795d47342c549
Ygor Vaz <mr.ygorvaz@gmail.com>
2021-03-23 20:32:59 -0300

    Add D-Pad buttons in SDF mode

--- sys/DsHid.c
+++ sys/DsHid.c
@@ -168,15 +168,15 @@ CONST HID_REPORT_DESCRIPTOR G_Ds3HidReportDescriptor_Single_Mode[] =
 	0x65, 0x00,        //   Unit (None)
 	0x05, 0x09,        //   Usage Page (Button)
 	0x19, 0x01,        //   Usage Minimum (0x01)
-	0x29, 0x0D,        //   Usage Maximum (0x0D)
+	0x29, 0x11,        //   Usage Maximum (0x11)
 	0x15, 0x00,        //   Logical Minimum (0)
 	0x25, 0x01,        //   Logical Maximum (1)
 	0x75, 0x01,        //   Report Size (1)
-	0x95, 0x0D,        //   Report Count (13)
+	0x95, 0x11,        //   Report Count (17)
 	0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
 	0x06, 0x00, 0xFF,  //   Usage Page (Vendor Defined 0xFF00)
 	0x09, 0x20,        //   Usage (0x20)
-	0x75, 0x07,        //   Report Size (7)
+	0x75, 0x03,        //   Report Size (3)
 	0x95, 0x01,        //   Report Count (1)
 	0x15, 0x00,        //   Logical Minimum (0)
 	0x25, 0x7F,        //   Logical Maximum (127)
@@ -792,8 +792,11 @@ VOID DS3_RAW_TO_SINGLE_HID_INPUT_REPORT(
 	// Prepare face buttons
 	Output[5] &= ~0xF0; // Clear upper 4 bits
 
-	// Remaining buttons
+	// Prepare buttons: L2, R2, L1, R1, L3, R3, Select and Start
 	Output[6] &= ~0xFF; // Clear all 8 bits
+
+	// Prepare PS and D-Pad buttons
+	Output[7] &= ~0xFF; // Clear all 8 bits
 
 	if (!MuteDigitalPressureButtons)
 	{
@@ -852,7 +855,10 @@ VOID DS3_RAW_TO_SINGLE_HID_INPUT_REPORT(
 	Output[9] = Input[19]; // R2
 
 	// PS button
-	Output[7] = Input[4]; // OUTPUT: PADDING [7-1], PS BUTTON [0]
+	Output[7] = Input[4] & 0x1; // OUTPUT: PS BUTTON [0]
+
+	// D-Pad buttons
+	Output[7] |= (Input[2] & ~0xF) >> 3; // OUTPUT: LEFT [4], DOWN [3], RIGHT [2], UP [1]
 
 	// D-Pad (pressure)
 	Output[10] = Input[14]; // UP
