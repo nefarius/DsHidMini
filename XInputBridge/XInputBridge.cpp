905d287e1b73b3bce07dcae5f55d0ac4b9d6db34
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-04-18 01:54:57 +0200

    Added function definitions

--- XInputBridge/XInputBridge.cpp
+++ XInputBridge/XInputBridge.cpp
@@ -5,17 +5,102 @@
 #include "XInputBridge.h"
 
 
-// This is an example of an exported variable
-XINPUTBRIDGE_API int nXInputBridge=0;
 
-// This is an example of an exported function.
-XINPUTBRIDGE_API int fnXInputBridge(void)
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetExtended(
+	_In_ DWORD dwUserIndex, 
+	_Out_ SCP_EXTN* pState
+)
 {
-    return 0;
+    return ERROR_DEVICE_NOT_CONNECTED;
 }
 
-// This is the constructor of a class that has been exported.
-CXInputBridge::CXInputBridge()
+XINPUTBRIDGE_API DWORD WINAPI XInputGetState(
+	_In_ DWORD dwUserIndex, 
+	_Out_ XINPUT_STATE* pState
+)
 {
-    return;
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputSetState(
+	_In_ DWORD dwUserIndex,
+	_In_ XINPUT_VIBRATION* pVibration
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetCapabilities(
+	_In_ DWORD dwUserIndex,
+	_In_ DWORD dwFlags,
+	_Out_ XINPUT_CAPABILITIES* pCapabilities
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API void WINAPI XInputEnable(
+	_In_ BOOL enable
+)
+{
+	return;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetDSoundAudioDeviceGuids(
+	DWORD dwUserIndex,
+	GUID* pDSoundRenderGuid,
+	GUID* pDSoundCaptureGuid
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetBatteryInformation(
+	_In_ DWORD dwUserIndex,
+	_In_ BYTE devType,
+	_Out_ XINPUT_BATTERY_INFORMATION* pBatteryInformation
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetKeystroke(
+	DWORD dwUserIndex,
+	DWORD dwReserved,
+	PXINPUT_KEYSTROKE pKeystroke
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetStateEx(
+	_In_ DWORD dwUserIndex,
+	_Out_ XINPUT_STATE* pState
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputWaitForGuideButton(
+	_In_ DWORD dwUserIndex,
+	_In_ DWORD dwFlag,
+	_In_ LPVOID pVoid
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputCancelGuideButtonWait(
+	_In_ DWORD dwUserIndex
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
+}
+
+XINPUTBRIDGE_API DWORD WINAPI XInputPowerOffController(
+	_In_ DWORD dwUserIndex
+)
+{
+	return ERROR_DEVICE_NOT_CONNECTED;
 }
--- XInputBridge/XInputBridge.h
+++ XInputBridge/XInputBridge.h
@@ -10,13 +10,203 @@
 #define XINPUTBRIDGE_API __declspec(dllimport)
 #endif
 
-// This class is exported from the dll
-class XINPUTBRIDGE_API CXInputBridge {
-public:
-	CXInputBridge(void);
-	// TODO: add your methods here.
-};
 
-extern XINPUTBRIDGE_API int nXInputBridge;
+#pragma region XInput types
 
-XINPUTBRIDGE_API int fnXInputBridge(void);
+#define XINPUT_GAMEPAD_DPAD_UP          0x0001
+#define XINPUT_GAMEPAD_DPAD_DOWN        0x0002
+#define XINPUT_GAMEPAD_DPAD_LEFT        0x0004
+#define XINPUT_GAMEPAD_DPAD_RIGHT       0x0008
+#define XINPUT_GAMEPAD_START            0x0010
+#define XINPUT_GAMEPAD_BACK             0x0020
+#define XINPUT_GAMEPAD_LEFT_THUMB       0x0040
+#define XINPUT_GAMEPAD_RIGHT_THUMB      0x0080
+#define XINPUT_GAMEPAD_LEFT_SHOULDER    0x0100
+#define XINPUT_GAMEPAD_RIGHT_SHOULDER   0x0200
+#define XINPUT_GAMEPAD_A                0x1000
+#define XINPUT_GAMEPAD_B                0x2000
+#define XINPUT_GAMEPAD_X                0x4000
+#define XINPUT_GAMEPAD_Y				0x8000
+
+#define XINPUT_CAPS_FFB_SUPPORTED       0x0001
+#define XINPUT_CAPS_WIRELESS            0x0002
+#define XINPUT_CAPS_PMD_SUPPORTED       0x0008
+#define XINPUT_CAPS_NO_NAVIGATION       0x0010
+
+//
+// Flags for battery status level
+//
+#define BATTERY_TYPE_DISCONNECTED       0x00    // This device is not connected
+#define BATTERY_TYPE_WIRED              0x01    // Wired device, no battery
+#define BATTERY_TYPE_ALKALINE           0x02    // Alkaline battery source
+#define BATTERY_TYPE_NIMH               0x03    // Nickel Metal Hydride battery source
+#define BATTERY_TYPE_UNKNOWN            0xFF    // Cannot determine the battery type
+
+// These are only valid for wireless, connected devices, with known battery types
+// The amount of use time remaining depends on the type of device.
+#define BATTERY_LEVEL_EMPTY             0x00
+#define BATTERY_LEVEL_LOW               0x01
+#define BATTERY_LEVEL_MEDIUM            0x02
+#define BATTERY_LEVEL_FULL              0x03
+
+
+#define XINPUT_DEVTYPE_GAMEPAD          0x01
+#define XINPUT_DEVSUBTYPE_GAMEPAD       0x01
+
+#define BATTERY_TYPE_DISCONNECTED		0x00
+
+#define XUSER_MAX_COUNT                 4
+#define MAX_PLAYER_COUNT				8
+#define XUSER_INDEX_ANY					0x000000FF
+
+//
+// Structures used by XInput APIs
+//
+typedef struct _XINPUT_GAMEPAD
+{
+	WORD                                wButtons;
+	BYTE                                bLeftTrigger;
+	BYTE                                bRightTrigger;
+	SHORT                               sThumbLX;
+	SHORT                               sThumbLY;
+	SHORT                               sThumbRX;
+	SHORT                               sThumbRY;
+} XINPUT_GAMEPAD, * PXINPUT_GAMEPAD;
+
+typedef struct _XINPUT_STATE
+{
+	DWORD                               dwPacketNumber;
+	XINPUT_GAMEPAD                      Gamepad;
+} XINPUT_STATE, * PXINPUT_STATE;
+
+typedef struct _XINPUT_VIBRATION
+{
+	WORD                                wLeftMotorSpeed;
+	WORD                                wRightMotorSpeed;
+} XINPUT_VIBRATION, * PXINPUT_VIBRATION;
+
+typedef struct _XINPUT_CAPABILITIES
+{
+	BYTE                                Type;
+	BYTE                                SubType;
+	WORD                                Flags;
+	XINPUT_GAMEPAD                      Gamepad;
+	XINPUT_VIBRATION                    Vibration;
+} XINPUT_CAPABILITIES, * PXINPUT_CAPABILITIES;
+
+typedef struct _XINPUT_BATTERY_INFORMATION
+{
+	BYTE BatteryType;
+	BYTE BatteryLevel;
+} XINPUT_BATTERY_INFORMATION, * PXINPUT_BATTERY_INFORMATION;
+
+typedef struct _XINPUT_KEYSTROKE
+{
+	WORD    VirtualKey;
+	WCHAR   Unicode;
+	WORD    Flags;
+	BYTE    UserIndex;
+	BYTE    HidCode;
+} XINPUT_KEYSTROKE, * PXINPUT_KEYSTROKE;
+
+#pragma endregion
+
+
+/**
+ * SCP custom report format for DualShock 3 Controllers.
+ *
+ * @author	Benjamin "Nefarius" Höglinger-Stelzer
+ * @date	18.04.2021
+ */
+typedef struct _SCP_EXTN {
+	FLOAT SCP_UP;
+	FLOAT SCP_RIGHT;
+	FLOAT SCP_DOWN;
+	FLOAT SCP_LEFT;
+
+	FLOAT SCP_LX;
+	FLOAT SCP_LY;
+
+	FLOAT SCP_L1;
+	FLOAT SCP_L2;
+	FLOAT SCP_L3;
+
+	FLOAT SCP_RX;
+	FLOAT SCP_RY;
+
+	FLOAT SCP_R1;
+	FLOAT SCP_R2;
+	FLOAT SCP_R3;
+
+	FLOAT SCP_T;
+	FLOAT SCP_C;
+	FLOAT SCP_X;
+	FLOAT SCP_S;
+
+	FLOAT SCP_SELECT;
+	FLOAT SCP_START;
+
+	FLOAT SCP_PS;
+} SCP_EXTN, * PSCP_EXTN;
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetExtended(
+	_In_ DWORD dwUserIndex,
+	_Out_ SCP_EXTN* pState
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetState(
+	_In_ DWORD dwUserIndex, 
+	_Out_ XINPUT_STATE* pState
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputSetState(
+	_In_ DWORD dwUserIndex,
+	_In_ XINPUT_VIBRATION* pVibration
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetCapabilities(
+	_In_ DWORD dwUserIndex,
+	_In_ DWORD dwFlags,
+	_Out_ XINPUT_CAPABILITIES* pCapabilities
+);
+
+XINPUTBRIDGE_API void WINAPI XInputEnable(
+	_In_ BOOL enable
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetDSoundAudioDeviceGuids(
+	DWORD dwUserIndex,
+	GUID* pDSoundRenderGuid,
+	GUID* pDSoundCaptureGuid
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetBatteryInformation(
+	_In_ DWORD dwUserIndex,
+	_In_ BYTE devType,
+	_Out_ XINPUT_BATTERY_INFORMATION* pBatteryInformation
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetKeystroke(
+	DWORD dwUserIndex,
+	DWORD dwReserved,
+	PXINPUT_KEYSTROKE pKeystroke
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputGetStateEx(
+	_In_ DWORD dwUserIndex, 
+	_Out_ XINPUT_STATE* pState
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputWaitForGuideButton(
+	_In_ DWORD dwUserIndex,
+	_In_ DWORD dwFlag,
+	_In_ LPVOID pVoid
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputCancelGuideButtonWait(
+	_In_ DWORD dwUserIndex
+);
+
+XINPUTBRIDGE_API DWORD WINAPI XInputPowerOffController(
+	_In_ DWORD dwUserIndex
+);
