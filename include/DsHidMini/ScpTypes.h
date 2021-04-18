31b180fd24c1dd9909a9cd391c49504aa3a58e5e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-04-18 14:54:38 +0200

    Added type SCP_EXTN

--- include/DsHidMini/ScpTypes.h
+++ include/DsHidMini/ScpTypes.h
@@ -1,1 +1,40 @@
 #pragma once
+
+#include <pshpack1.h>
+/**
+ * SCP custom report format for DualShock 3 Controllers.
+ *
+ * @author	Benjamin "Nefarius" Höglinger-Stelzer
+ * @date	18.04.2021
+ */
+typedef struct _SCP_EXTN {
+	FLOAT SCP_UP;
+	FLOAT SCP_RIGHT;
+	FLOAT SCP_DOWN;
+	FLOAT SCP_LEFT;
+
+	FLOAT SCP_LX;
+	FLOAT SCP_LY;
+
+	FLOAT SCP_L1;
+	FLOAT SCP_L2;
+	FLOAT SCP_L3;
+
+	FLOAT SCP_RX;
+	FLOAT SCP_RY;
+
+	FLOAT SCP_R1;
+	FLOAT SCP_R2;
+	FLOAT SCP_R3;
+
+	FLOAT SCP_T;
+	FLOAT SCP_C;
+	FLOAT SCP_X;
+	FLOAT SCP_S;
+
+	FLOAT SCP_SELECT;
+	FLOAT SCP_START;
+
+	FLOAT SCP_PS;
+} SCP_EXTN, * PSCP_EXTN;
+#include <poppack.h>
