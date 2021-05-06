6a5a184f39a147ec20269cc089eabca3b2d09d1a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-05-06 16:14:09 +0200

    Disabled ability to select XInput mode when running DirectInput variant

---
+++ DSHMC/Util/WPF/HidModeComboBoxDisableConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Nefarius.DsHidMini.Drivers;
+
+namespace Nefarius.DsHidMini.Util.WPF
+{
+    public class HidModeComboBoxDisableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            // You can add your custom logic here to disable combobox item
+            return (DsHidDeviceMode) value == DsHidDeviceMode.XInputHIDCompatible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
