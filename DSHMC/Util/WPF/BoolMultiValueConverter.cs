4f55222ee639a0d9fcb08964ecbf7437cd978950
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-07-02 13:41:43 +0200

    Addresses #101

---
+++ DSHMC/Util/WPF/BoolMultiValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Nefarius.DsHidMini.Util.WPF
+{
+    internal class BoolMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return values.Cast<bool>().All(v => v);
+            }
+            catch (InvalidCastException)
+            {
+                return false;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
