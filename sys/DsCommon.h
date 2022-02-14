82e84ec5b5d8ae881f56f8a74f206e5db71a1730
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-02-14 21:54:04 +0100

    Added DS_PRESSURE_EXPOSURE_MODE

--- sys/DsCommon.h
+++ sys/DsCommon.h
@@ -7,7 +7,7 @@
 #include <pshpack1.h>
 typedef struct _BD_ADDR
 {
-    UCHAR Address[6];
+	UCHAR Address[6];
 
 } BD_ADDR, * PBD_ADDR;
 #include <poppack.h>
@@ -17,17 +17,17 @@ typedef struct _BD_ADDR
 // 
 typedef enum
 {
-    //
-    // Unknown device type
-    // 
-    DsDeviceTypeUnknown,
+	//
+	// Unknown device type
+	// 
+	DsDeviceTypeUnknown,
 
-    //
-    // Sony DualShock 3 Controller
-    // 
-    DsDeviceTypeSixaxis,
+	//
+	// Sony DualShock 3 Controller
+	// 
+	DsDeviceTypeSixaxis,
 
-    //
+	//
 	// Sony Navigation Controller
 	// 
 	DsDeviceTypeNavigation,
@@ -36,11 +36,11 @@ typedef enum
 	// Sony Motion Controller
 	// 
 	DsDeviceTypeMotion,
-	
-    //
-    // Sony DualShock 4 Controller
-    // 
-    DsDeviceTypeWireless
+
+	//
+	// Sony DualShock 4 Controller
+	// 
+	DsDeviceTypeWireless
 
 } DS_DEVICE_TYPE, * PDS_DEVICE_TYPE;
 
@@ -49,9 +49,9 @@ typedef enum
 // 
 typedef enum
 {
-    DsDeviceConnectionTypeUnknown = 0x00,
-    DsDeviceConnectionTypeUsb,
-    DsDeviceConnectionTypeBth
+	DsDeviceConnectionTypeUnknown = 0x00,
+	DsDeviceConnectionTypeUsb,
+	DsDeviceConnectionTypeBth
 
 } DS_CONNECTION_TYPE, * PDS_CONNECTION_TYPE;
 
@@ -60,28 +60,28 @@ typedef enum
 // 
 typedef enum
 {
-    DsBatteryStatusNone = 0x00,
-    DsBatteryStatusDying = 0x01,
-    DsBatteryStatusLow = 0x02,
-    DsBatteryStatusMedium = 0x03,
-    DsBatteryStatusHigh = 0x04,
-    DsBatteryStatusFull = 0x05,
-    DsBatteryStatusCharging = 0xEE,
-    DsBatteryStatusCharged = 0xEF
-	
-} DS_BATTERY_STATUS, *PDS_BATTERY_STATUS;
+	DsBatteryStatusNone = 0x00,
+	DsBatteryStatusDying = 0x01,
+	DsBatteryStatusLow = 0x02,
+	DsBatteryStatusMedium = 0x03,
+	DsBatteryStatusHigh = 0x04,
+	DsBatteryStatusFull = 0x05,
+	DsBatteryStatusCharging = 0xEE,
+	DsBatteryStatusCharged = 0xEF
+
+} DS_BATTERY_STATUS, * PDS_BATTERY_STATUS;
 
 //
 // See https://vigem.org/projects/DsHidMini/HID-Device-Modes-Explained
 // 
 typedef enum
 {
-    DsHidMiniDeviceModeUnknown = 0x00,
-    DsHidMiniDeviceModeSDF,
-    DsHidMiniDeviceModeGPJ,
-    DsHidMiniDeviceModeSixaxisCompatible,
-    DsHidMiniDeviceModeDS4WindowsCompatible,
-    DsHidMiniDeviceModeXInputHIDCompatible
+	DsHidMiniDeviceModeUnknown = 0x00,
+	DsHidMiniDeviceModeSDF,
+	DsHidMiniDeviceModeGPJ,
+	DsHidMiniDeviceModeSixaxisCompatible,
+	DsHidMiniDeviceModeDS4WindowsCompatible,
+	DsHidMiniDeviceModeXInputHIDCompatible
 
 } DS_HID_DEVICE_MODE, * PDS_HID_DEVICE_MODE;
 
@@ -93,12 +93,12 @@ typedef enum
 	//
 	// DSHM controls output report generation
 	// 
-    Ds3OutputReportModeDriverHandled = 0,
+	Ds3OutputReportModeDriverHandled = 0,
 
 	//
 	// Output reports come in from "the outside" and get passed on
 	// 
-    Ds3OutputReportModeWriteReportPassThrough
+	Ds3OutputReportModeWriteReportPassThrough
 
 } DS_OUTPUT_REPORT_MODE, * PDS_OUTPUT_REPORT_MODE;
 
@@ -110,34 +110,54 @@ typedef enum
 	//
 	// Request originated from ourself (must not be discarded)
 	// 
-    Ds3OutputReportSourceDriverHighPriority = 0,
+	Ds3OutputReportSourceDriverHighPriority = 0,
 
-    //
-    // Request originated from ourself (may be discarded)
-    // 
-    Ds3OutputReportSourceDriverLowPriority,
+	//
+	// Request originated from ourself (may be discarded)
+	// 
+	Ds3OutputReportSourceDriverLowPriority,
 
 	//
 	// Request came in through Force Feedback exposure
 	// 
-    Ds3OutputReportSourceForceFeedback,
+	Ds3OutputReportSourceForceFeedback,
 
 	//
 	// Request came in through SXS exposure
 	// 
-    Ds3OutputReportSourcePassThrough,
+	Ds3OutputReportSourcePassThrough,
 
 	//
 	// Request came in from DS4 emulation
 	// 
-    Ds3OutputReportSourceDualShock4,
+	Ds3OutputReportSourceDualShock4,
 
 	//
 	// Request came from XINPUTHID.SYS
 	// 
-    Ds3OutputReportSourceXInputHID
+	Ds3OutputReportSourceXInputHID
 
 } DS_OUTPUT_REPORT_SOURCE, * PDS_OUTPUT_REPORT_SOURCE;
+
+//
+// Flags of whether pressure sensitive button variants should be exposed as digital, analogue or both
+// 
+typedef enum
+{
+	//
+	// Default behaviour (depends on device mode)
+	// 
+	DsPressureExposureModeDefault = 0,
+	//
+	// Only report digital (two-state) pressure sensitive button state changes
+	// 
+	DsPressureExposureModeDigitalOnly = 1 << 0,
+	//
+	// Only report analogue (range) pressure sensitive button state changes
+	// 
+	DsPressureExposureModeAnalogueOnly = 1 << 1
+
+} DS_PRESSURE_EXPOSURE_MODE, * PDS_PRESSURE_EXPOSURE_MODE;
 
 //
 // Per device dynamic configuration properties
@@ -145,32 +165,35 @@ typedef enum
 #include <pshpack1.h>
 typedef struct _DS_DRIVER_CONFIGURATION
 {
-    /** The HID device mode */
-    DS_HID_DEVICE_MODE HidDeviceMode;
-	
-    /** True to mute digital pressure buttons */
-    BOOLEAN MuteDigitalPressureButtons;
+	/** The HID device mode */
+	DS_HID_DEVICE_MODE HidDeviceMode;
+
+	/** True to mute digital pressure buttons */
+	BOOLEAN MuteDigitalPressureButtons;
 
 	/** When set, pairing will not be attempted on device boot */
 	BOOLEAN DisableAutoPairing;
 
-    /** True if output rate control is enabled, false if not */
-    BOOLEAN IsOutputRateControlEnabled;
+	/** True if output rate control is enabled, false if not */
+	BOOLEAN IsOutputRateControlEnabled;
 
-    /** The output rate control period in milliseconds */
-    UCHAR OutputRateControlPeriodMs;
+	/** The output rate control period in milliseconds */
+	UCHAR OutputRateControlPeriodMs;
 
-    /** True if output deduplicator is enabled, false if not */
-    UCHAR IsOutputDeduplicatorEnabled;
+	/** True if output deduplicator is enabled, false if not */
+	UCHAR IsOutputDeduplicatorEnabled;
 
-    /** Idle disconnect period in milliseconds */
-    ULONG WirelessIdleTimeoutPeriodMs;
+	/** Idle disconnect period in milliseconds */
+	ULONG WirelessIdleTimeoutPeriodMs;
 
-    struct
-    {
-        BOOL test;
+	//
+	// SDF-mode specific
+	// 
+	struct
+	{
+		DS_PRESSURE_EXPOSURE_MODE PressureExposureMode;
 
-    } SDF;
-	
+	} SDF;
+
 } DS_DRIVER_CONFIGURATION, * PDS_DRIVER_CONFIGURATION;
 #include <poppack.h>
