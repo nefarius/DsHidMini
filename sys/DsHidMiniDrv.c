3f520d1e91d1384a915aaaf2eb41d0569d81a54e
Ygor Vaz <mr.ygorvaz@gmail.com>
2022-02-18 01:38:04 -0300

    Implemented prototype rumble function in DS4W mode

--- sys/DsHidMiniDrv.c
+++ sys/DsHidMiniDrv.c
@@ -1048,8 +1048,11 @@ DsHidMini_WriteReport(
 
 		if (isSetRumble)
 		{
+			DS3_SET_BOTH_RUMBLE_STRENGTH(pDevCtx, Packet->reportBuffer[5], Packet->reportBuffer[4]);
+			/*
 			DS3_SET_SMALL_RUMBLE_STRENGTH(pDevCtx, Packet->reportBuffer[4]);
 			DS3_SET_LARGE_RUMBLE_STRENGTH(pDevCtx, Packet->reportBuffer[5]);
+			*/
 		}
 
 		if (isSetColor)
