e5e0714161176b1c0a5e01ef09b0b9da5e41c66d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-08-22 16:17:54 +0200

    Enabled ARM64 build Fixed stage0.ps1 script

--- stage0.ps1
+++ stage0.ps1
@@ -10,7 +10,6 @@ Param(
 ) #end param
 
 $signTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool.exe"
-$crossCert = "C:\Program Files (x86)\Windows Kits\10\CrossCertificates\DigiCert_High_Assurance_EV_Root_CA.crt"
 $timestampUrl = "http://timestamp.digicert.com"
 $certName = "Nefarius Software Solutions e.U."
 
@@ -105,6 +104,9 @@ function Get-AppVeyorArtifacts
     }
 }
 
+# Download ARM64 binaries
+Get-AppVeyorArtifacts -Account "nefarius" -Project "DsHidMini" -Path $Path -Token $Token -Branch $BuildVersion -JobName "Platform: ARM64"
+
 # Download x64 binaries
 Get-AppVeyorArtifacts -Account "nefarius" -Project "DsHidMini" -Path $Path -Token $Token -Branch $BuildVersion -JobName "Platform: x64"
 
@@ -124,7 +126,7 @@ if ($NoSigning -eq $false) {
     # remove existing certificate(s)
     Invoke-Expression "& `"$signTool`" remove /s $files"
     # sign with only one certificate
-    Invoke-Expression "& `"$signTool`" sign /v /ac `"$crossCert`" /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"
+    Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"
 }
 
-Compress-Archive -Path ".\artifacts\bin\*" -DestinationPath ".\artifacts\dshidmini_v$BuildVersion.zip"
+#Compress-Archive -Path ".\artifacts\bin\*" -DestinationPath ".\artifacts\dshidmini_v$BuildVersion.zip"
