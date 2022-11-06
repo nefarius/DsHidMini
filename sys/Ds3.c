0ccb2e68c10ee3d5d9396b7fde3fb06e537e8afa
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-11-06 19:04:03 +0100

    Added comment

--- sys/Ds3.c
+++ sys/Ds3.c
@@ -400,6 +400,9 @@ VOID DS3_GET_UNIFIED_OUTPUT_REPORT_BUFFER(
 	}
 }
 
+//
+// Gets output report without offset
+// 
 VOID DS3_GET_RAW_OUTPUT_REPORT_BUFFER(
 	PDEVICE_CONTEXT Context,
 	UCHAR** Buffer,
