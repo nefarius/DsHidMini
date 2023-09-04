689ef056b2d9907df5b1cc923557c678340e8132
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-09-04 20:28:19 +0200

    Update MainWindow.xaml.cs

--- ControlApp/Views/Windows/MainWindow.xaml.cs
+++ ControlApp/Views/Windows/MainWindow.xaml.cs
@@ -4,88 +4,94 @@
 // All Rights Reserved.
 
 using System.ComponentModel;
-using Microsoft.Extensions.DependencyInjection;
+
 using Nefarius.DsHidMini.ControlApp.Models.Drivers;
-using Nefarius.DsHidMini.ControlApp.Models.DshmConfigManager;
-using Nefarius.DsHidMini.ControlApp.ViewModels;
-using Nefarius.DsHidMini.ControlApp.ViewModels.Pages;
 using Nefarius.DsHidMini.ControlApp.ViewModels.Windows;
 using Nefarius.Utilities.DeviceManagement.PnP;
+
 using Wpf.Ui;
+using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
-namespace Nefarius.DsHidMini.ControlApp.Views.Windows
+namespace Nefarius.DsHidMini.ControlApp.Views.Windows;
+
+public partial class MainWindow : INavigationWindow
 {
-    public partial class MainWindow : INavigationWindow
+    private readonly DeviceNotificationListener _listener;
+
+    public MainWindow(
+        MainWindowViewModel viewModel,
+        DeviceNotificationListener listener, //
+        INavigationService navigationService,
+        IServiceProvider serviceProvider,
+        ISnackbarService snackbarService,
+        IContentDialogService contentDialogService
+    )
     {
-        private readonly DeviceNotificationListener _listener;
-        public MainWindowViewModel ViewModel { get; }
-        
-        public MainWindow(
-            MainWindowViewModel viewModel,
-            DeviceNotificationListener listener,//
-            INavigationService navigationService,
-            IServiceProvider serviceProvider,
-            ISnackbarService snackbarService,
-            IContentDialogService contentDialogService
-        )
-        {
- 
+        ViewModel = viewModel;
+        DataContext = this;
 
-            ViewModel = viewModel;
-            DataContext = this;
+        _listener = listener;
 
-            _listener = listener;
-            
-            Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
+        SystemThemeWatcher.Watch(this);
 
-            InitializeComponent();
+        InitializeComponent();
 
-            navigationService.SetNavigationControl(RootNavigation);
-            snackbarService.SetSnackbarPresenter(SnackbarPresenter);
-            contentDialogService.SetContentPresenter(RootContentDialog);
-
-            
-
-
-        }
-
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-
-            InitializeComponent();
-            _listener.StartListen(DsHidMiniDriver.DeviceInterfaceGuid);
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            base.OnClosing(e);
-
-            _listener.StopListen();
-            _listener.Dispose();
-        }
-
-
-        
-
-        #region INavigationWindow methods
-
-        public INavigationView GetNavigation() => RootNavigation;
-
-        public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
-
-        public void SetPageService(IPageService pageService) =>
-            RootNavigation.SetPageService(pageService);
-
-        public void ShowWindow() => Show();
-
-        public void CloseWindow() => Close();
-
-        public void SetServiceProvider(IServiceProvider serviceProvider)
-        {
-            throw new NotImplementedException();
-        }
-        #endregion INavigationWindow methods
+        navigationService.SetNavigationControl(RootNavigation);
+        snackbarService.SetSnackbarPresenter(SnackbarPresenter);
+        contentDialogService.SetContentPresenter(RootContentDialog);
     }
+
+    public MainWindowViewModel ViewModel { get; }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        InitializeComponent();
+        _listener.StartListen(DsHidMiniDriver.DeviceInterfaceGuid);
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        base.OnClosing(e);
+
+        _listener.StopListen();
+        _listener.Dispose();
+    }
+
+
+    #region INavigationWindow methods
+
+    public INavigationView GetNavigation()
+    {
+        return RootNavigation;
+    }
+
+    public bool Navigate(Type pageType)
+    {
+        return RootNavigation.Navigate(pageType);
+    }
+
+    public void SetPageService(IPageService pageService)
+    {
+        RootNavigation.SetPageService(pageService);
+    }
+
+    public void ShowWindow()
+    {
+        Show();
+    }
+
+    public void CloseWindow()
+    {
+        Close();
+    }
+
+    public void SetServiceProvider(IServiceProvider serviceProvider)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion INavigationWindow methods
 }
