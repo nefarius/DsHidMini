0af38a90a0257981fe07a5f9d55067b00818c3f4
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-07 00:08:56 +0200

    Added new setup skeleton

---
+++ setup/InstallScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using WixSharp;
+
+namespace Nefarius.DsHidMini.Setup;
+
+internal class InstallScript
+{
+    static void Main()
+    {
+        var project = new Project("MyProduct",
+            new Dir(@"%ProgramFiles%\My Company\My Product",
+                new File("Program.cs")));
+
+        project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+        //project.SourceBaseDir = "<input dir path>";
+        //project.OutDir = "<output dir path>";
+
+        project.BuildMsi();
+    }
+}
