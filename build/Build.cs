82c621ea8f7b3e90550c43d7cfa6299708c59aa9
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-02-18 14:21:22 +0100

    Updated nuke build system to latest version (8.0.0)
    Updated build scripts to use .NET 8

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.AppVeyor;
@@ -10,12 +11,13 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Utilities.Collections;
+
+using Serilog;
+
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
-using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
-[CheckBuildProjectConfigurations]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -23,16 +25,15 @@ class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
+    [Solution]
+    readonly Solution Solution;
 
-    AbsolutePath DmfSolution => ((AbsolutePath)"C:/projects/DMF/Dmf.sln");
+    AbsolutePath DmfSolution => (AbsolutePath)"C:/projects/DMF/Dmf.sln";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -43,45 +44,43 @@ class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
-            MSBuild(s => s
-                .SetTargetPath(Solution)
-                .SetTargets("Restore"));
         });
 
     Target BuildDmf => _ => _
-    .Executes(() =>
-    {
-        if (IsLocalBuild)
-            return;
-
-        Console.WriteLine($"DMF solution path: {DmfSolution}");
-
-        var platform = AppVeyor.Instance.Platform switch
-        {
-            "x86" => MSBuildTargetPlatform.Win32,
-            "ARM64" => (MSBuildTargetPlatform)"ARM64",
-            _ => MSBuildTargetPlatform.x64
-        };
-
-        MSBuild(s => s
-            .SetTargetPath(DmfSolution)
-            .SetTargets("Build")
-            .SetConfiguration(Configuration)
-            .SetTargetPlatform(platform)
-            .SetMaxCpuCount(Environment.ProcessorCount)
-            .SetNodeReuse(IsLocalBuild));
-    });
-
-    Target Compile => _ => _
-        .DependsOn(BuildDmf)
         .Executes(() =>
         {
-            MSBuild(s => s
+            if (IsLocalBuild)
+            {
+                return;
+            }
+
+            Log.Information("DMF solution path: {DmfSolution}", DmfSolution);
+
+            MSBuildTargetPlatform platform = AppVeyor.Instance.Platform switch
+            {
+                "x86" => MSBuildTargetPlatform.Win32,
+                "ARM64" => (MSBuildTargetPlatform)"ARM64",
+                _ => MSBuildTargetPlatform.x64
+            };
+
+            MSBuildTasks.MSBuild(s => s
+                .SetTargetPath(DmfSolution)
+                .SetTargets("Build")
+                .SetConfiguration(Configuration)
+                .SetTargetPlatform(platform)
+                .SetMaxCpuCount(Environment.ProcessorCount)
+                .SetNodeReuse(IsLocalBuild));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            MSBuildTasks.MSBuild(s => s
                 .SetTargetPath(Solution)
                 .SetTargets("Rebuild")
                 .SetConfiguration(Configuration)
                 .SetMaxCpuCount(Environment.ProcessorCount)
                 .SetNodeReuse(IsLocalBuild));
         });
-
 }
