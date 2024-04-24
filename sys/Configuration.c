cf06ee44bc7c2b3a02b57df53d87c360b42090bb
KxK <mr.ygorvaz@gmail.com>
2024-04-24 15:11:53 -0300

    Update default values for user's rumble settings

--- sys/Configuration.c
+++ sys/Configuration.c
@@ -943,18 +943,18 @@ ConfigSetDefaults(
 	Config->ThumbSettings.DeadZoneRight.Apply = TRUE;
 	Config->ThumbSettings.DeadZoneRight.PolarValue = 3.0;
 
-	Config->RumbleSettings.DisableBM = FALSE;
-	Config->RumbleSettings.DisableSM = FALSE;
-	Config->RumbleSettings.BMStrRescale.Enabled = TRUE;
-	Config->RumbleSettings.BMStrRescale.MinValue = 64;
-	Config->RumbleSettings.BMStrRescale.MaxValue = 255;
-	Config->RumbleSettings.SMToBMConversion.Enabled = FALSE;
-	Config->RumbleSettings.SMToBMConversion.RescaleMinValue = 1;
-	Config->RumbleSettings.SMToBMConversion.RescaleMaxValue = 140;
-	Config->RumbleSettings.ForcedSM.BMThresholdEnabled = TRUE;
-	Config->RumbleSettings.ForcedSM.BMThresholdValue = 230;
-	Config->RumbleSettings.ForcedSM.SMThresholdEnabled = FALSE;
-	Config->RumbleSettings.ForcedSM.SMThresholdValue = 230;
+	Config->RumbleSettings.DisableLeft = FALSE;
+	Config->RumbleSettings.DisableRight = FALSE;
+	Config->RumbleSettings.HeavyRescaling.IsEnabled = TRUE;
+	Config->RumbleSettings.HeavyRescaling.MinRange = 64;
+	Config->RumbleSettings.HeavyRescaling.MaxRange = 255;
+	Config->RumbleSettings.AlternativeMode.IsEnabled = FALSE;
+	Config->RumbleSettings.AlternativeMode.MinRange = 1;
+	Config->RumbleSettings.AlternativeMode.MaxRange = 140;
+	Config->RumbleSettings.AlternativeMode.ForcedRight.IsHeavyThresholdEnabled = TRUE;
+	Config->RumbleSettings.AlternativeMode.ForcedRight.HeavyThreshold = 230;
+	Config->RumbleSettings.AlternativeMode.ForcedRight.IsLightThresholdEnabled = FALSE;
+	Config->RumbleSettings.AlternativeMode.ForcedRight.LightThreshold = 230;
 
 	Config->LEDSettings.Mode = DsLEDModeBatteryIndicatorPlayerIndex;
 	Config->LEDSettings.CustomPatterns.LEDFlags = 0x02;
