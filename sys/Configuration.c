694701ab9b47c24183d2e0698762c8ef717d916e
KxK <mr.ygorvaz@gmail.com>
2024-04-26 07:52:56 -0300

    Adjust default value

--- sys/Configuration.c
+++ sys/Configuration.c
@@ -963,7 +963,7 @@ ConfigSetDefaults(
 	Config->RumbleSettings.HeavyRescaling.MaxRange = 255;
 	Config->RumbleSettings.AlternativeMode.IsEnabled = FALSE;
 	Config->RumbleSettings.AlternativeMode.MinRange = 1;
-	Config->RumbleSettings.AlternativeMode.MaxRange = 110;
+	Config->RumbleSettings.AlternativeMode.MaxRange = 90;
 	Config->RumbleSettings.AlternativeMode.ForcedRight.IsHeavyThresholdEnabled = TRUE;
 	Config->RumbleSettings.AlternativeMode.ForcedRight.HeavyThreshold = 242;
 	Config->RumbleSettings.AlternativeMode.ForcedRight.IsLightThresholdEnabled = FALSE;
