66613d8fd4b465ea5a7b16abd9da47b65a073653
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-05-07 03:09:00 +0200

    Experiments

--- setup/InstallScript.cs
+++ setup/InstallScript.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Windows.Forms;
+
+using Microsoft.Deployment.WindowsInstaller;
+
+using Nefarius.Utilities.DeviceManagement.PnP;
 
 using WixSharp;
 
@@ -6,16 +11,36 @@ namespace Nefarius.DsHidMini.Setup;
 
 internal class InstallScript
 {
-    static void Main()
+    private static void Main()
     {
-        var project = new Project("MyProduct",
-            new Dir(@"%ProgramFiles%\My Company\My Product",
-                new File("Program.cs")));
+        Project project = new("DsHidMini",
+            new Dir(@"%ProgramFiles%\Nefarius Software Solutions\DsHidMini",
+                new File("InstallScript.cs")),
+            new ManagedAction(CustomActions.MyManagedAction, Return.check,
+                When.After,
+                Step.InstallInitialize,
+                Condition.NOT_Installed));
 
         project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+
+        project.DefaultRefAssemblies.Add(typeof(Devcon).Assembly.Location);
+
         //project.SourceBaseDir = "<input dir path>";
         //project.OutDir = "<output dir path>";
 
         project.BuildMsi();
+    }
+}
+
+public static class CustomActions
+{
+    [CustomAction]
+    public static ActionResult MyManagedAction(Session session)
+    {
+        bool found = Devcon.FindByInterfaceGuid(DeviceInterfaceIds.HidDevice, out _);
+
+        MessageBox.Show(found.ToString(), "Embedded Managed CA");
+
+        return ActionResult.Success;
     }
 }
