e187fdbfa41072bbec7316e01fed15222d76a3d6
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-05-22 16:28:46 +0200

    Added GetLogger method

--- XInputBridge/GlobalState.Util.cpp
+++ XInputBridge/GlobalState.Util.cpp
@@ -282,4 +282,10 @@ nostd::shared_ptr<trace::Tracer> GlobalState::GetTracer()
 	const auto provider = trace::Provider::GetTracerProvider();
 	return provider->GetTracer(TRACER_NAME, OPENTELEMETRY_SDK_VERSION);
 }
+
+nostd::shared_ptr<logs::Logger> GlobalState::GetLogger(const std::string& name)
+{
+	const auto provider = logs::Provider::GetLoggerProvider();
+	return provider->GetLogger(name, LOGGER_NAME);
+}
 #endif
