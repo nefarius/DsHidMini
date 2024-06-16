178c3ad5a1765b13e3636cfcce1c06156becefef
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-06-16 19:32:20 +0200

    Migrated DSHM_SetFeature

--- sys/DsHidMiniDrv.c
+++ sys/DsHidMiniDrv.c
@@ -491,124 +491,12 @@ DsHidMini_SetFeature(
 {
 	FuncEntry(TRACE_DSHIDMINIDRV);
 
-	NTSTATUS status = STATUS_SUCCESS;
-
 	UNREFERENCED_PARAMETER(Request);
 
+	const PDEVICE_CONTEXT pDevCtx = DeviceGetContext(DMF_ParentDeviceGet(DmfModule));
 	const DMF_CONTEXT_DsHidMini* pModCtx = DMF_CONTEXT_GET(DMF_ParentModuleGet(DmfModule));
 	
-#ifdef DSHM_FEATURE_FFB
-
-	FFB_ATTRIBUTES ffbEntry = { 0 };
-
-	PPID_NEW_EFFECT_REPORT pNewEffect;
-
-	switch (Packet->reportId)
-	{
-	case PID_NEW_EFFECT_REPORT_ID:
-
-		pNewEffect = (PPID_NEW_EFFECT_REPORT)Packet->reportBuffer;
-
-		TraceVerbose(TRACE_DSHIDMINIDRV, "!! PID_CREATE_NEW_EFFECT_REPORT");
-
-	//
-	// Look for free effect block index and allocate new entry
-	// 
-		for (UCHAR index = 1; index < MAX_EFFECT_BLOCKS; index++)
-		{
-			status = DMF_HashTable_Read(
-				pModCtx->DmfModuleForceFeedback,
-				&index,
-				sizeof(UCHAR),
-				(PUCHAR)&ffbEntry,
-				sizeof(FFB_ATTRIBUTES),
-				NULL
-			);
-
-			if (status == STATUS_NOT_FOUND || !ffbEntry.IsReserved)
-			{
-				ffbEntry.EffectBlockIndex = index; // next free index
-				ffbEntry.EffectType = pNewEffect->EffectType; // effect type requested
-				ffbEntry.IsReserved = TRUE; // mark as reserved/allocated
-				ffbEntry.IsReported = FALSE; // index allocated but not claimed yet
-
-				status = DMF_HashTable_Write(
-					pModCtx->DmfModuleForceFeedback,
-					&index,
-					sizeof(UCHAR),
-					(PUCHAR)&ffbEntry,
-					sizeof(FFB_ATTRIBUTES)
-				);
-
-				break;
-			}
-		}
-
-	//
-	// Whoops, guess we're full!
-	// 
-		if (!ffbEntry.IsReserved)
-		{
-			EventWriteFFBNoFreeEffectBlockIndex();
-			status = STATUS_INVALID_PARAMETER;
-			break;
-		}
-
-		switch (pNewEffect->EffectType)
-		{
-		case PidEtConstantForce:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Constant Force");
-			break;
-		case PidEtRamp:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Ramp");
-			break;
-		case PidEtSquare:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Square");
-			break;
-		case PidEtSine:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Sine");
-			break;
-		case PidEtTriangle:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Triangle");
-			break;
-		case PidEtSawtoothUp:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Sawtooth Up");
-			break;
-		case PidEtSawtoothDown:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Sawtooth Down");
-			break;
-		case PidEtSpring:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Spring");
-			break;
-		case PidEtDamper:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Damper");
-			break;
-		case PidEtInertia:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Inertia");
-			break;
-		case PidEtFriction:
-			TraceVerbose(TRACE_DSHIDMINIDRV, "!! ET Friction");
-			break;
-		}
-
-		*ReportSize = Packet->reportBufferLen;
-
-		break;
-	default:
-		TraceEvents(TRACE_LEVEL_WARNING,
-			TRACE_DSHIDMINIDRV, "%!FUNC! Not implemented");
-
-		TraceVerbose(TRACE_DSHIDMINIDRV, "-- Packet->reportId: %d, Packet->reportBufferLen: %d",
-			Packet->reportId,
-			Packet->reportBufferLen);
-
-		DumpAsHex("-- SET_FEATURE.reportBuffer", Packet->reportBuffer, Packet->reportBufferLen);
-		break;
-	}
-
-#else
-	UNREFERENCED_PARAMETER(pModCtx);
-#endif
+	NTSTATUS status = DSHM_SetFeature(pDevCtx, pModCtx, Packet, ReportSize);
 
 	FuncExit(TRACE_DSHIDMINIDRV, "status=%!STATUS!", status);
 
--- sys/FeatureReport.h
+++ sys/FeatureReport.h
@@ -9,3 +9,13 @@ DSHM_GetFeature(
 	_In_ HID_XFER_PACKET* Packet,
 	_Out_ ULONG* ReportSize
 );
+
+_Must_inspect_result_
+_Success_(return == STATUS_SUCCESS)
+NTSTATUS
+DSHM_SetFeature(
+	_In_ const PDEVICE_CONTEXT DeviceContext,
+	_In_ const DMF_CONTEXT_DsHidMini* ModuleContext,
+	_In_ HID_XFER_PACKET* Packet,
+	_Out_ ULONG* ReportSize
+);
