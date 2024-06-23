79def5c2e40dbe9aa41887f062086eeb81ba644e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-06-23 23:45:24 +0200

    Added setup build step

--- stage2.ps1
+++ stage2.ps1
@@ -1,9 +1,16 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$SetupVersion
+) #end param
+
 $signTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe"
 $timestampUrl = "http://timestamp.digicert.com"
 $certName = "Nefarius Software Solutions e.U."
 
 # List of files to sign
 $files =    "`".\setup\*.msi`" "
+
+dotnet build -c:Release -p:SetupVersion=$SetupVersion .\setup\DsHidMini.Installer.csproj
 
 # sign with only one certificate
 Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"
