5ba6358651ef644ce71dc57f5fc7027447f48cdc
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-06-23 20:19:07 +0200

    Driver installation works

--- setup/InstallScript.cs
+++ setup/InstallScript.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 using Microsoft.Deployment.WindowsInstaller;
 
@@ -18,8 +20,11 @@ internal class InstallScript
 {
     private static void Main()
     {
+        // grab main app version
+        Version version = Version.Parse("3.0.0"); // TODO: make configurable
+
         ManagedProject project = new("DsHidMini",
-            new Dir(@"%ProgramFiles%\Nefarius Software Solutions\DsHidMini",
+            new InstallDir(@"%ProgramFiles%\Nefarius Software Solutions\DsHidMini",
                 new Dir("drivers",
                     new Files(@"..\artifacts\drivers\*.*"),
                     new Files(@"..\artifacts\igfilter\*.*")
@@ -27,19 +32,34 @@ internal class InstallScript
             ),
             new ManagedAction(CustomActions.InstallDrivers, Return.check,
                 When.After,
-                Step.InstallInitialize,
+                Step.InstallFinalize,
                 Condition.NOT_Installed)
-        );
+        )
+        {
+            Version = version,
+            Platform = Platform.x64,
+            GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b"),
+            LicenceFile = "EULA.rtf",
+            WildCardDedup = Project.UniqueFileNameDedup,
+            MajorUpgradeStrategy = MajorUpgradeStrategy.Default
+        };
 
-        project.Platform = Platform.x64;
-        project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
-        project.LicenceFile = "EULA.rtf";
         // embed types of Nefarius.Utilities.DeviceManagement
         project.DefaultRefAssemblies.Add(typeof(Devcon).Assembly.Location);
         project.AfterInstall += ProjectOnAfterInstall;
 
         //project.SourceBaseDir = "<input dir path>";
         //project.OutDir = "<output dir path>";
+
+        project.ControlPanelInfo.ProductIcon =
+            Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\assets\FireShock.ico");
+        project.ControlPanelInfo.Manufacturer = "Nefarius Software Solutions e.U.";
+        project.ControlPanelInfo.HelpLink = "https://docs.nefarius.at/Community-Support/";
+        project.ControlPanelInfo.UrlInfoAbout = "https://github.com/nefarius/DsHidMini";
+
+        project.MajorUpgradeStrategy.PreventDowngradingVersions.OnlyDetect = false;
+
+        project.ResolveWildCards();
 
         project.BuildMsi();
     }
@@ -64,9 +84,38 @@ public static class CustomActions
     [CustomAction]
     public static ActionResult InstallDrivers(Session session)
     {
+        // clean out whatever has been on the machine before
         bool rebootRequired = UninstallDrivers();
 
-        // TODO: implement me!
+        DirectoryInfo installDir = new(session.Property("INSTALLDIR"));
+        string driversDir = Path.Combine(installDir.FullName, "drivers");
+        string archShortName = RuntimeInformation.OSArchitecture.ToString();
+
+        string dshidminiDriverDir = Path.Combine(driversDir, $"dshidmini_{archShortName}");
+        string igfilterDriverDir = Path.Combine(driversDir, $"nssmkig_{archShortName}");
+
+        string dshidminiInfPath = Path.Combine(dshidminiDriverDir, "dshidmini.inf");
+        string igfilterInfPath = Path.Combine(igfilterDriverDir, "igfilter.inf");
+
+        if (!Devcon.Install(igfilterInfPath, out bool igfilterRebootRequired))
+        {
+            return ActionResult.Failure;
+        }
+
+        if (igfilterRebootRequired)
+        {
+            rebootRequired = true;
+        }
+
+        if (!Devcon.Install(dshidminiInfPath, out bool dshidminiRebootRequired))
+        {
+            return ActionResult.Failure;
+        }
+
+        if (dshidminiRebootRequired)
+        {
+            rebootRequired = true;
+        }
 
         Devcon.Refresh();
 
