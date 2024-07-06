040a348155834118dc7a56fec6342a44906f085d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-07-06 20:28:17 +0200

    Removed legacy code
    Replaced outdated URL

--- ControlApp/Models/Util/Web/Validator.cs
+++ ControlApp/Models/Util/Web/Validator.cs
@@ -1,60 +1,75 @@
-﻿using System.Net;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using System.Net.NetworkInformation;
+
 using Newtonsoft.Json;
 
-namespace Nefarius.DsHidMini.ControlApp.Models.Util.Web
+namespace Nefarius.DsHidMini.ControlApp.Models.Util.Web;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class OUIEntry : IEquatable<OUIEntry>
 {
-    public class OUIEntry : IEquatable<OUIEntry>
+    public OUIEntry(string manufacturer)
     {
-        public OUIEntry(string manufacturer)
+        string hex = manufacturer.Replace(":", string.Empty);
+
+        Bytes = Enumerable.Range(0, hex.Length / 2).Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16))
+            .ToArray();
+    }
+
+    public OUIEntry(PhysicalAddress address)
+    {
+        Bytes = address.GetAddressBytes().Take(3).ToArray();
+    }
+
+    public byte[] Bytes { get; }
+
+    public bool Equals(OUIEntry other)
+    {
+        if (ReferenceEquals(null, other))
         {
-            var hex = manufacturer.Replace(":", string.Empty);
-
-            Bytes = Enumerable.Range(0, hex.Length / 2).Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16))
-                .ToArray();
-        }
-
-        public OUIEntry(PhysicalAddress address)
-        {
-            Bytes = address.GetAddressBytes().Take(3).ToArray();
-        }
-
-        public byte[] Bytes { get; }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is OUIEntry entry)
-                return Bytes.SequenceEqual(entry.Bytes);
             return false;
         }
 
-        public bool Equals(OUIEntry other)
+        if (ReferenceEquals(this, other))
         {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Bytes.SequenceEqual(other.Bytes);
+            return true;
         }
 
-        public override int GetHashCode()
-        {
-            return Bytes.GetHashCode();
-        }
+        return Bytes.SequenceEqual(other.Bytes);
     }
 
-    public class Validator
+    public override bool Equals(object obj)
     {
-        public static Uri ApiUrl => new Uri("https://vigem.org/projects/DsHidMini/genuine_oui_db.json");
-
-        public static bool IsGenuineAddress(PhysicalAddress address)
+        if (obj is OUIEntry entry)
         {
-            using (var client = new WebClient())
-            {
-                var ouiList = JsonConvert.DeserializeObject<IList<string>>(client.DownloadString(ApiUrl)).Select(e => new OUIEntry(e));
+            return Bytes.SequenceEqual(entry.Bytes);
+        }
 
-                var device = new OUIEntry(address);
+        return false;
+    }
 
-                return ouiList.Contains(device);
-            }
+    public override int GetHashCode()
+    {
+        return Bytes.GetHashCode();
+    }
+}
+
+public class Validator
+{
+    public static Uri ApiUrl => new("https://docs.nefarius.at/projects/DsHidMini/genuine_oui_db.json");
+
+    [Obsolete("Redesign to use modern HttpClient instead.")]
+    public static bool IsGenuineAddress(PhysicalAddress address)
+    {
+        using (WebClient client = new WebClient())
+        {
+            IEnumerable<OUIEntry> ouiList = JsonConvert.DeserializeObject<IList<string>>(client.DownloadString(ApiUrl))
+                .Select(e => new OUIEntry(e));
+
+            OUIEntry device = new OUIEntry(address);
+
+            return ouiList.Contains(device);
         }
     }
 }
