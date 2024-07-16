fbf07054c9e502965f58a3dbd8982afd20656f43
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-07-16 16:28:52 +0200

    Replaced obsolete method call

--- ControlApp/Views/Windows/MainWindow.xaml.cs
+++ ControlApp/Views/Windows/MainWindow.xaml.cs
@@ -4,98 +4,94 @@
 // All Rights Reserved.
 
 using System.ComponentModel;
-using Microsoft.Extensions.DependencyInjection;
+
 using Nefarius.DsHidMini.ControlApp.Models;
-using Nefarius.DsHidMini.ControlApp.Models.Drivers;
 using Nefarius.DsHidMini.ControlApp.ViewModels.Windows;
-using Nefarius.Utilities.DeviceManagement.PnP;
 
 using Wpf.Ui;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
-namespace Nefarius.DsHidMini.ControlApp.Views.Windows
+namespace Nefarius.DsHidMini.ControlApp.Views.Windows;
+
+public partial class MainWindow : INavigationWindow
 {
-    public partial class MainWindow : INavigationWindow
+    private readonly DshmDevMan _dshmDevMan;
+
+    public MainWindow(
+        MainWindowViewModel viewModel,
+        DshmDevMan dshmDevMan, //
+        INavigationService navigationService,
+        IServiceProvider serviceProvider,
+        ISnackbarService snackbarService,
+        IContentDialogService contentDialogService
+    )
     {
-        private readonly DshmDevMan _dshmDevMan;
-        public MainWindowViewModel ViewModel { get; }
+        ViewModel = viewModel;
+        DataContext = this;
 
-        public MainWindow(
-            MainWindowViewModel viewModel,
-            DshmDevMan dshmDevMan, //
-            INavigationService navigationService,
-            IServiceProvider serviceProvider,
-            ISnackbarService snackbarService,
-            IContentDialogService contentDialogService
-        )
-        {
+        _dshmDevMan = dshmDevMan;
 
+        SystemThemeWatcher.Watch(this);
 
-            ViewModel = viewModel;
-            DataContext = this;
+        ApplicationThemeManager.Apply(ApplicationTheme.Dark);
 
-            _dshmDevMan = dshmDevMan;
+        InitializeComponent();
 
-            Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
-
-            ApplicationThemeManager.Apply(ApplicationTheme.Dark);
-
-            InitializeComponent();
-
-            navigationService.SetNavigationControl(RootNavigation);
-            snackbarService.SetSnackbarPresenter(SnackbarPresenter);
-            contentDialogService.SetContentPresenter(RootContentDialog);
-        }
-
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-
-            InitializeComponent();
-            _dshmDevMan.StartListeningForDshmDevices();
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            base.OnClosing(e);
-
-            _dshmDevMan.StopListeningForDshmDevices();
-        }
-
-
-        #region INavigationWindow methods
-
-        public INavigationView GetNavigation()
-        {
-            return RootNavigation;
-        }
-
-        public bool Navigate(Type pageType)
-        {
-            return RootNavigation.Navigate(pageType);
-        }
-
-        public void SetPageService(IPageService pageService)
-        {
-            RootNavigation.SetPageService(pageService);
-        }
-
-        public void ShowWindow()
-        {
-            Show();
-        }
-
-        public void CloseWindow()
-        {
-            Close();
-        }
-
-        public void SetServiceProvider(IServiceProvider serviceProvider)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion INavigationWindow methods
+        navigationService.SetNavigationControl(RootNavigation);
+        snackbarService.SetSnackbarPresenter(SnackbarPresenter);
+        contentDialogService.SetDialogHost(RootContentDialog);
     }
+
+    public MainWindowViewModel ViewModel { get; }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        InitializeComponent();
+        _dshmDevMan.StartListeningForDshmDevices();
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        base.OnClosing(e);
+
+        _dshmDevMan.StopListeningForDshmDevices();
+    }
+
+
+    #region INavigationWindow methods
+
+    public INavigationView GetNavigation()
+    {
+        return RootNavigation;
+    }
+
+    public bool Navigate(Type pageType)
+    {
+        return RootNavigation.Navigate(pageType);
+    }
+
+    public void SetPageService(IPageService pageService)
+    {
+        RootNavigation.SetPageService(pageService);
+    }
+
+    public void ShowWindow()
+    {
+        Show();
+    }
+
+    public void CloseWindow()
+    {
+        Close();
+    }
+
+    public void SetServiceProvider(IServiceProvider serviceProvider)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion INavigationWindow methods
 }
