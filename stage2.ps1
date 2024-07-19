5c532357e2e62bcbcdc5d44243ded3c21ae27669
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-07-19 19:29:10 +0200

    Improved setup script

--- stage2.ps1
+++ stage2.ps1
@@ -7,10 +7,7 @@ $signTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtoo
 $timestampUrl = "http://timestamp.digicert.com"
 $certName = "Nefarius Software Solutions e.U."
 
-# List of files to sign
-$files =    "`".\setup\*.msi`" "
-
 dotnet build -c:Release -p:SetupVersion=$SetupVersion .\setup\DsHidMini.Installer.csproj
 
 # sign with only one certificate
-Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"
+Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 .\setup\Nefarius_DsHidMini_Drivers_x64_arm64_v$SetupVersion.msi"
