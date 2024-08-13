82527c7b0e5a069416d29dfa83a46c3b8b05432a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-13 21:58:28 +0200

    Namespace fixes

---
+++ ipctest/Models/Enums.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nefarius.DsHidMini.IPC.Models;
+
+// Describes the type of IPC message response behavior
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public enum DSHM_IPC_MSG_TYPE : UInt32
+{
+    // Invalid/reserved, do not use
+    DSHM_IPC_MSG_TYPE_INVALID = 0,
+
+    // Client-to-driver data incoming, no acknowledgment/reply requested
+    DSHM_IPC_MSG_TYPE_REQUEST_ONLY,
+
+    // Client-to-driver data incoming, must be acknowledged by reply
+    DSHM_IPC_MSG_TYPE_REQUEST_RESPONSE,
+
+    // Client requested data, there is nothing to read for the driver
+    DSHM_IPC_MSG_TYPE_RESPONSE_ONLY
+}
+
+// Describes the message receiver
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public enum DSHM_IPC_MSG_TARGET : UInt32
+{
+    // Invalid/reserved, do not use
+    DSHM_IPC_MSG_TARGET_INVALID = 0,
+
+    // The message is targeted at the driver
+    DSHM_IPC_MSG_TARGET_DRIVER,
+
+    // The message is targeted at a device
+    DSHM_IPC_MSG_TARGET_DEVICE
+}
+
+// Describes a per-driver command
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public enum DSHM_IPC_MSG_CMD_DRIVER : UInt32
+{
+    // Invalid/reserved, do not use
+    DSHM_IPC_MSG_CMD_DRIVER_INVALID = 0
+}
+
+// Describes a per-device command
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public enum DSHM_IPC_MSG_CMD_DEVICE : UInt32
+{
+    // Invalid/reserved, do not use
+    DSHM_IPC_MSG_CMD_DEVICE_INVALID = 0,
+
+    // Pair a given device to a new host
+    DSHM_IPC_MSG_CMD_DEVICE_PAIR_TO
+}
---
+++ ipctest/Models/Structs.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Nefarius.DsHidMini.IPC.Models;
+
+[StructLayout(LayoutKind.Explicit)]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public struct DSHM_IPC_MSG_COMMAND
+{
+    [FieldOffset(0)]
+    public DSHM_IPC_MSG_CMD_DRIVER Driver;
+
+    [FieldOffset(0)]
+    public DSHM_IPC_MSG_CMD_DEVICE Device;
+}
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public struct DSHM_IPC_MSG_HEADER
+{
+    // What request-behavior is expected (request, request-reply, ...)
+    public DSHM_IPC_MSG_TYPE Type;
+
+    // What component is this message targeting (driver, device, ...)
+    public DSHM_IPC_MSG_TARGET Target;
+
+    // What command is this message carrying
+    public DSHM_IPC_MSG_COMMAND Command;
+
+    // One-based index of which device is this message for
+    // Set to 0 if driver is targeted
+    public uint TargetIndex;
+
+    // The size of the entire message (header + payload) in bytes
+    // A size of 0 is invalid
+    public uint Size;
+}
