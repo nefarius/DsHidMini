919ea04372847843ccb8d601c95d31aa385c7603
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-14 20:58:32 +0200

    Update Program.cs

--- ipctest/Program.cs
+++ ipctest/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using System.Net.NetworkInformation;
 
 using Nefarius.DsHidMini.IPC;
@@ -10,7 +11,31 @@ if (!DsHidMiniInterop.IsAvailable)
     return;
 }
 
-using var ipc = new DsHidMiniInterop();
+using DsHidMiniInterop ipc = new();
 
-//ipc.SendPing();
+// Create a Stopwatch instance to track time
+Stopwatch stopwatch = new();
+
+// Counter for the number of method executions
+int executionCount = 0;
+
+// Start the stopwatch
+stopwatch.Start();
+
+// Loop until the elapsed time is 1 second (1000 milliseconds)
+while (stopwatch.ElapsedMilliseconds < 1000)
+{
+    // Call the method you want to execute
+    ipc.SendPing();
+
+    // Increment the counter
+    executionCount++;
+}
+
+// Stop the stopwatch
+stopwatch.Stop();
+
+// Output the number of executions in one second
+Console.WriteLine($"Executed {executionCount} PINGs in one second.");
+
 Console.WriteLine(ipc.SetHostAddress(1, PhysicalAddress.Parse("C0:13:37:DE:AD:BE")));
